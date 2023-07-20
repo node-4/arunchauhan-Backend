f95884d1da5e3d1908c9e5195e5f3b6a6b52c1ed
rubiyadav <rubiflyeis@gmail.com>
2023-07-20 14:35:41 +0530

    create an api

--- src/Controller/review.js
+++ src/Controller/review.js
@@ -2,39 +2,42 @@ const review = require('../Models/review')
 
 
 
-exports.AddReview = async(req,res) => {
-    try{
+exports.AddReview = async (req, res) => {
+  try {
     const data = {
-        userId: req.body.userId, 
-        productId: req.body.productId, 
-        rating: parseInt(req.body.rating)
+      userId: req.body.userId,
+      productId: req.body.productId,
+      rating: parseInt(req.body.rating),
+      message: req.body.message
     }
-    const result = await review.create(data).populate('userId', 'productId');
-    res.status(200).json({message: "Review is Added ", data:result})
-    }catch(err){
-        console.log(err.message);
-      res.status(500).send({ msg: "internal server error ", error: err.message });
-    }
+    const result = await review.create(data)
+    const Review = await review.findById(result._id).populate('userId productId');
+
+    res.status(200).json({ message: "Review is Added ", data: Review })
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send({ msg: "internal server error ", error: err.message });
+  }
 }
 
 
 
-exports.getAll = async(req,res) => {
-    try{
-    const data = await review.find().populate(['userId', "productId"])
+exports.getAll = async (req, res) => {
+  try {
+    const data = await review.find().populate('userId productId')
     res.status(200).json({
-        message: "ok",
-        data: data
+      message: "ok",
+      data: data
     })
-}catch(err){
-        console.log(err.message);
-      res.status(500).send({ msg: "internal server error ", error: err.message });
-    }
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send({ msg: "internal server error ", error: err.message });
+  }
 }
 
 
 // Get a single review by ID
-exports.getReviewById = async(req,res) => {
+exports.getReviewById = async (req, res) => {
   try {
     const Review = await review.findById(req.params.id);
     return res.status(200).send(Review);
@@ -44,13 +47,13 @@ exports.getReviewById = async(req,res) => {
 };
 
 // Update a review by ID
-exports.updateReview = async(req,res) => {
+exports.updateReview = async (req, res) => {
   try {
 
-    const update ={
-        userId: req.body.userId, 
-        productId: req.body.productId, 
-        rating: parseInt(req.body.rating)
+    const update = {
+      userId: req.body.userId,
+      productId: req.body.productId,
+      rating: parseInt(req.body.rating)
     }
     const Review = await review.findByIdAndUpdate(req.params.id, update, {
       new: true,
@@ -62,7 +65,7 @@ exports.updateReview = async(req,res) => {
 };
 
 // Delete a review by ID
-exports.deleteReview = async(req,res) => {
+exports.deleteReview = async (req, res) => {
   try {
     const Review = await review.findByIdAndDelete(req.params.id);
     return res.status(200).send(Review);
--- src/Models/review.js
+++ src/Models/review.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
-
-
-
 const review = mongoose.Schema({
     userId: {
-        type: mongoose.Schema.Types.ObjectId, 
+        type: mongoose.Schema.Types.ObjectId,
         ref: "user"
-    }, 
+    },
     productId: {
-        type: mongoose.Schema.Types.ObjectId, 
+        type: mongoose.Schema.Types.ObjectId,
         ref: "product"
-    }, 
+    },
+    message: {
+        type: String,
+    },
     rating: {
-        type: Number, 
+        type: Number,
         default: 1
     }
 })
