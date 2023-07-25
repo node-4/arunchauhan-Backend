2f3779b4fe800619fdfa4e270015dabc33510f91
rubiyadav <rubiflyeis@gmail.com>
2023-07-25 19:16:58 +0530

    create an api card

--- src/Routes/SellerRoutes.js
+++ src/Routes/SellerRoutes.js
@@ -8,7 +8,7 @@ const cloudinary = require("cloudinary").v2;
 cloudinary.config({ cloud_name: "dbrvq9uxa", api_key: "567113285751718", api_secret: "rjTsz9ksqzlDtsrlOPcTs_-QtW4", });
 const storage = new CloudinaryStorage({ cloudinary: cloudinary, params: { folder: "images/image", allowed_formats: ["jpg", "jpeg", "png", "PNG", "xlsx", "xls", "pdf", "PDF"], }, });
 const upload = multer({ storage: storage });
-router.post('/',adminMiddleware,SellerController.addSeller);
+router.post('/',SellerController.addSeller);
 router.get('/',adminMiddleware,SellerController.getAllSeller);
 router.post('/signin',SellerController.sellerSignin);
 router.put('/',sellerSignin,upload.single('myField'),SellerController.updateSeller);
